7667d6889e762434df63258f25bf0fe68802a7ab
LordEmris <samueltaylor2209@gmail.com>
2023-09-14 18:51:17 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	char e;
+
+	for (e = '0'; e <= '9'; e++)
+	{
+	_putchar(e);
+	}
+	_putchar('\n');
+}
